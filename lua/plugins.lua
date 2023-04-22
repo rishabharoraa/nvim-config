cd3245ed338e506f5297fda33402462ec52299d3
rishabharoraa <rishabharora.ns@gmail.com>
2023-04-22 21:50:07 +0530

    added lsp config and switched to lsp-zero

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -12,8 +12,28 @@ local startupFunction = function(use)
 		require("toggleterm").setup()
 	end}
     use 'jiangmiao/auto-pairs'
+
     -- lspconfig
-    use 'neovim/nvim-lspconfig'
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+            'williamboman/mason.nvim',
+            run = function()
+                pcall(vim.cmd, 'MasonUpdate')
+            end,
+        },
+        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},     -- Required
+        {'hrsh7th/cmp-nvim-lsp'}, -- Required
+        {'L3MON4D3/LuaSnip'},     -- Required
+    }
+}
 end
 
 return require("packer").startup(startupFunction)
